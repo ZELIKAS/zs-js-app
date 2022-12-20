70833852b44eea7ea654e83d0f62b1e82ea72cd9
ZELIKAS <118128198+ZELIKAS@users.noreply.github.com>
2022-12-20 15:43:03 -0500

    Update scripts.js
    created a new variable called pokemonList; to which i added several arrays to it. I then added 3 keys to each pokemon object: name, height and type

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let pokemonList = [charizard, oddish, jigglypuff]
+let charizrd = {
+    name: "Charizard", height: 5.07, type: ["fire", "flying"]
+}
+let oddish = {
+    name: "Oddish", height: 1.08, type: ["grass", "poison"]
+}
+let jigglypuff = {
+    name: "Jigglypuff", height: 1.08, type: ["normal", "fairy"]
+}
