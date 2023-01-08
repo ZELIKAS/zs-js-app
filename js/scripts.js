560f57569bc3d2ea4d219ab3db275fe6799ccf99
ZELIKAS <118128198+ZELIKAS@users.noreply.github.com>
2023-01-08 16:43:57 -0500

    Update scripts.js
    Changed the way of listing to give it a more specific/concise order.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,5 @@
-let pokemonList = [charizard, oddish, jigglypuff]
-let charizard = {
-    name: "Charizard", height: 5.07, type: ["fire", "flying"]
-}
-let oddish = {
-    name: "Oddish", height: 1.08, type: ["grass", "poison"]
-}
-let jigglypuff = {
-    name: "Jigglypuff", height: 1.08, type: ["normal", "fairy"]
-}
+let pokemonList = [
+    { ID :"1", name: "Charizard", height: 5.07, type: ["fire", "flying"]},
+    { ID :"2", name: "Oddish", height: 1.08, type: ["grass", "poison"]},
+    { ID :"3", name: "Jigglypuff", height: 1.08, type: ["normal", "fairy"]}
+  ]
